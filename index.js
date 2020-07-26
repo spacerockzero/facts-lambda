e6e47acab686a6299ae4413621070fd185cc19a1
Jakob Anderson <janderson@familysearch.org>
2020-07-25 20:08:51 -0600

    return random fact

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const facts = require('facts/data/facts.json')
 console.log('facts.length', facts.length)
 
 exports.handler = async (event) => {
+  const randomFact = facts[Math.floor(Math.random() * facts.length)];
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
+    body: JSON.stringify(randomFact),
   };
   return response;
 };
