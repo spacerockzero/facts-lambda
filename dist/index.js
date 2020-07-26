7ffbc76ef5457467fdf476d6e89694dc0d04fd12
Jakob Anderson <janderson@familysearch.org>
2020-07-26 13:08:52 -0600

    removed dep, package.json and zip file so action can build it

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,13 @@
-const facts = require('./facts.json')
+const facts = require('facts/data/facts.json')
 
 console.log('facts.length', facts.length)
 
-exports.handler = async (event, something, callback) => {
+exports.handler = async (event) => {
   const randomFact = facts[Math.floor(Math.random() * facts.length)];
+  // TODO implement
   const response = {
     statusCode: 200,
     body: JSON.stringify(randomFact),
   };
-  return callback(response);
+  return response;
 };
