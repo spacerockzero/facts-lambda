09dee8424ef95a73c1a5d09b7a617a797cb6fa12
Jakob Anderson <janderson@familysearch.org>
2020-07-26 13:10:03 -0600

    use working version of index

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const facts = require('./facts.json')
 
 console.log('facts.length', facts.length)
 
-exports.handler = async (event, something, callback) => {
+exports.handler = async (event, context, callback) => {
   const randomFact = facts[Math.floor(Math.random() * facts.length)];
   const response = {
     statusCode: 200,
     body: JSON.stringify(randomFact),
   };
-  return callback(response);
+  return callback(null, response);
 };
