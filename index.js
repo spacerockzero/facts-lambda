15a02502f86cfcaadfe56ce3607182463e48ea15
Jakob Anderson <janderson@familysearch.org>
2020-07-27 20:38:27 -0600

    added familymode

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const facts = require('./facts.json')
+const factsFamily = require('./factsFamily.json')
 
 console.log('facts.length', facts.length)
 
 exports.handler = async (event, context, callback) => {
-  const randomFact = facts[Math.floor(Math.random() * facts.length)];
+  const familymode = event['pathParameters']['familymode']
+
+  const randomFact = familymode
+    ? facts[Math.floor(Math.random() * facts.length)]
+    : factsFamily[Math.floor(Math.random() * factsFamily.length)]
   const response = {
     statusCode: 200,
     body: JSON.stringify(randomFact),
