ea14811f51e5ed08b399995edc15fa9211089cac
Jakob Anderson <janderson@familysearch.org>
2020-07-28 08:44:44 -0600

    querystring should be optional

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const { handler } = require('./index')
 describe('test lambda', () => {
   it('should respond with random fact when no params are used', () => {
     // no param
-    handler({ pathParameters: {} }, null, (err, res) => {
+    handler(null, null, (err, res) => {
       console.log(err, res)
       expect(err).to.equal(null)
       expect(res).to.exist
@@ -12,7 +12,7 @@ describe('test lambda', () => {
   })
   it('should respond with random familymode fact when param is used', () => {
     // familymode param
-    handler({ pathParameters: { 'familymode': true } }, null, (err, res) => {
+    handler({ queryStringParameters: { 'familymode': true } }, null, (err, res) => {
       console.log(err, res)
       expect(err).to.equal(null)
       expect(res).to.exist
